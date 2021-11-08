2016cd3dbf14b500ebdf71999bc32d7cbef0fb28
Snehal_Adhe <snehaladhe4768@gmail.com>
2021-11-08 14:22:22 +0530

    init

---
+++ src/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const d = new Date();
+const year = d.getFullYear();
+const Footer = () => {
+  return (
+    <>
+      <footer className="footer mt-auto py-3 bg-dark ">
+        <div className="container">
+          <span className="text-muted">
+            <center>
+              <p className="fclass">copyright © {year}</p>
+            </center>
+          </span>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
---
+++ src/Gallary.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+
+const Gallary = () =>{
+    return <h1>THIS IS Gallary PAGE</h1>
+};
+export default Gallary;
